7eeb90c6352cfa8b60631a8908ea818cccd5c3ac
simon <ertel.simon@gmail.com>
2016-03-02 17:44:15 +0100

    build script

--- build.js
+++ build.js
@@ -2,22 +2,16 @@ const fs = require('fs')
 
 const LVLS_DIR = './sokobapp/www/js/lvl'
 
-var i = 0;
-const lvls = fs.readdir(LVLS_DIR, (err, filenames) => {
-
-      filenames
-      	.filter(filename => /maps-[0-9]+\.js/.test(filename))
-        .map(function(filename){
-            i++;
-        	  fs.writeFileSync(`${LVLS_DIR}/world-${i}.json`,
-              JSON.stringify(
-                require(`${LVLS_DIR}/${filename}`)
-              )
-            );
-            fs.writeFileSync(
-              `${LVLS_DIR}/all-levels.json`,
-              `{"number":${i}}`
-            )
-        })
-
+fs.readdir(LVLS_DIR, (err, filenames) => {
+  const levels = (
+    filenames
+      .filter(filename => /maps-[0-9]+\.js/.test(filename))
+      .map((filename, i) => {
+        fs.writeFileSync(
+          `${LVLS_DIR}/world-${i}.json`,
+          JSON.stringify(require(`${LVLS_DIR}/${filename}`))
+        )
+      })
+  )
+  fs.writeFileSync(`${LVLS_DIR}/all-levels.json`, `{"number":${levels.length}}`)
 })
